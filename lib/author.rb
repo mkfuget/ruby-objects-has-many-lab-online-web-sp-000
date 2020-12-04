cdcfc0fe088767de567ba8fcbfced505cdb7c38b
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 02:52:33 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author 
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end 
+  
+  def add_post(new_post)
+    new_post.author = self
+  end
+  
+  def posts
+    return Post.all.select{|post| post.author == self}
+  end
+  
+  def self.post_count
+    return Song.all.length
+  end
+  
+  def add_post_by_title(new_name)
+    add_post(Song.new(new_name))
+  end
+    
+end
